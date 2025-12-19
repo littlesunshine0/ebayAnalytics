fee33dbb3f431f684edd2b45fa1bf0fb60ba9fbe
littlesunshine0 <gary_robert_ellis@icloud.com>
2025-12-19 15:25:12 -0800

    Delete Services directory

--- Services/SellAnalyticsServiceProtocol.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public protocol SellAnalyticsServiceProtocol: Sendable {
-    func getSellerStandardsProfile() async throws -> SellerStandardsProfile
-    func getTrafficReport(startDate: String, endDate: String) async throws -> TrafficReport
-    func getCustomerServiceMetric(evaluationType: String) async throws -> CustomerServiceMetric
-}
